574c8366c1a511510843fcf42b4cb9f673190f89
robbie632 <rob.632@hotmail.co.uk>
2021-04-23 17:04:14 +0100

    go tnowhere trying to dynamically render image in html

--- app/app.py
+++ app/app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from form import Form
+import os
+import sys
+import cv2
+sys.path.append("..")
+
+from utils import ClusterAbstract, MyKMeans
+
+import matplotlib.pyplot as plt, mpld3
 
 
 app = Flask(__name__)
@@ -12,8 +20,34 @@ app.config['SECRET_KEY'] = '15101964'
 def form():
   form = Form()
   if form.is_submitted():
-    answer = form.question.data
-    return redirect(url_for("landing"))
+
+    # #retrieve entered text
+    # answer = form.question.data
+
+    # #clustering
+    # ca = ClusterAbstract()
+    # ca.get(answer, #keyword query
+    #      '5'#number of abstracts
+    #   )
+    # ca.process_data()
+    # ca.encode_data()
+    # ca.cluster_data(3)
+
+    # ca.analyse_clusters()
+  
+    # ca.generate_word_clouds()
+    # print(f"clusters are {ca.model.labels}")
+
+    # #run cluster analysis
+    # print(type(ca.word_clouds[0]))
+
+    # ca.word_clouds[0].to_file("test.png")
+    img_pth = "test.png"
+    #img_pth = os.path.join("static", img_pth)
+    
+    print(os.getcwd())
+
+    return redirect(url_for("landing", img_pth=img_pth))
 
 
   else:
@@ -22,6 +56,17 @@ def form():
 @app.route('/landing', methods=["GET", "POST"])
 def landing():
   return render_template("landing.html")
+
+import cv2
+import base64
+
+def ndarray_to_b64(ndarray):
+    """
+    converts a np ndarray to a b64 string readable by html-img tags 
+    """
+    img = cv2.cvtColor(ndarray, cv2.COLOR_RGB2BGR)
+    _, buffer = cv2.imencode('.png', img)
+    return base64.b64encode(buffer).decode('utf-8')
 
 
 if __name__ == "__main__":
